cec7fb1e002839b6d17ffd7e44caceb2aa5fca2f
Anton Eliseev <anton2008.e@yandex.ru>
2021-01-02 23:52:14 +0400

    add quick sort function

---
+++ QuickSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuickSort
+{
+    class Program
+    {
+        public static List<int> QuickSort(List<int> list)
+        {
+            if (list.Count < 2)
+            {
+                return list;
+            }
+            else
+            {
+                var pivot = list[0];
+                list.Remove(pivot);
+                var less = list.Where(x => x <= pivot).ToList();
+                var greater = list.Where(x => x > pivot).ToList();
+                var resultList = QuickSort(less).Append(pivot).Concat(QuickSort(greater)).ToList();
+                return resultList;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var list = new List<int>{5,5,0,13,4,-4,-252,43,34,6,7,23,-13,-14};
+            foreach (var item in QuickSort(list))
+            {
+                Console.Write(item + " ");   
+            }
+        }
+    }
+}
