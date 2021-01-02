ea7d9c6379dd29506559209449cc9ea0c4690c68
Anton Eliseev <anton2008.e@yandex.ru>
2021-01-02 23:51:53 +0400

    add recursive sum function

---
+++ RecursiveFunctions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecursiveFunctions
+{
+    class Program
+    {
+        public static int Sum(List<int> list)
+        {
+            if (list.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                var x = list[0];
+                list.RemoveAt(0);
+                return x + Sum(list);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var list = new List<int>{1,2,3,4,5,6,7,8,9,-10}; //35
+            Console.WriteLine(Sum(list));
+        }
+    }
+}
